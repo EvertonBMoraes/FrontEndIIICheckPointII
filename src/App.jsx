7e54c0eacdedbff322cae1988e272578341d21ee
arianefrancaqa <arianefrancaqa@gmail.com>
2023-06-27 01:33:16 -0300

    adicionando routes e home

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,27 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
 
 function App() {
   return (
-    <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar a classe dark ou light */}
-      <div className={`app light}`}>
-        <Navbar />
-        <main>
-          <Outlet />
-        </main>
-        <Footer />
-      </div>
-    </>
-  );
+    <div>
+      <Navbar />
+      <Home />
+      {/* perguntar pro prof pq o Outlet nao esta aparecendo */}
+      <Outlet />
+      <Footer />
+    </div>
+    // <>
+    //   {/* //Na linha seguinte deverá ser feito um teste se a aplicação
+    //     // está em dark mode e deverá utilizar a classe dark ou light */}
+    //   <div className={`app light}`}>
+    //     <Navbar />
+    //     <Outlet />
+    //     <Footer />
+    //   </div>
+    // </>
+  )
 }
 
 export default App;
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,19 +1,48 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Card from "../Components/Card";
+import apiBaseUrl from "../api";
+import axios from "axios";
 
 const Home = () => {
+
+  const [dentistas, setdentistas] = useState([]);
+
+  const getDentistas = async () => {
+    const res = await axios(`${apiBaseUrl}/dentista`)
+    setdentistas(res.data)
+  }
 
   useEffect(() => {
     //Nesse useEffect, deverá ser obtido todos os dentistas da API
     //Armazena-los em um estado para posteriormente fazer um map
     //Usando o componente <Card />
+    console.log("<Home /> executou efeito colateral");
+    getDentistas();
+    return function unmount() {
+      console.log(console.log("<Character /> desmontou"));
+    }
   }, []);
 
   return (
     <>
       <h1>Home</h1>
       <div className="card-grid container">
-        <Card />
+        {
+
+          dentistas.map(dentista => {
+
+            return (
+              dentista ?
+
+                <Card
+                  nomeDentista={dentista.nome}
+                  matricula={dentista.matricula}
+                />
+
+                : <h2>A lista de dentistas está vazia</h2>
+            )
+          })
+        }
       </div>
     </>
   );
